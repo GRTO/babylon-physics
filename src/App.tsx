03e18cfa9ff507a88fffbd24a62da736da393831
Gerson Ossio <gossio@evolution.com>
2023-04-13 11:08:48 +0100

    Adding babylonJs with CannonJs

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,55 @@
+import { FC } from 'react';
+
+import "@babylonjs/core/Physics/physicsEngineComponent" // side-effect adds scene.enablePhysics function
+import "@babylonjs/core/Lights/Shadows/shadowGeneratorSceneComponent"; // side-effect for shadow generator
+import { Vector3 } from '@babylonjs/core/Maths/math.vector';
+import { CannonJSPlugin } from '@babylonjs/core/Physics';
+import { PhysicsImpostor } from '@babylonjs/core/Physics/physicsImpostor';
+import { Scene, Engine } from 'react-babylonjs';
+import { Color3 } from '@babylonjs/core';
+import * as CANNON from 'cannon';
+
+import { Sphere } from "./components/Sphere";
+
 import './app.scss';
 
-const App: React.FC = () => {
-  const name: string = 'Custom React App v18 - Typescript & Webpack 💻 👍 ';
-  return (
-    <div className="app-container">
-      <div className="workspace">
-        <h1 className="custom-styles">Hi, this is a boilerplate project called: {name}</h1>
-        <span>It uses:</span>
-        <ul>
-          <li>React v18</li>
-          <li>Typescript</li>
-          <li>Webpack</li>
-          <li>Babel</li>
-        </ul>
-      </div>
-    </div>
-  );
-};
+window.CANNON = CANNON;
+
+const RADIUS = 5;
+const NUMBER_OF_BOXES = 8;
+
+const gravityVector = new Vector3(0, -9.81, 0);
+
+const App: FC = () => {
+	return (
+		<div className="App">
+      <Engine antialias={true} adaptToDeviceRatio={true} canvasId="sample-canvas">
+        <Scene enablePhysics={[gravityVector, new CannonJSPlugin()]}>
+          <arcRotateCamera name="arc" target={new Vector3(0, 1, 0)}
+            alpha={-Math.PI / 2} beta={(0.2 + (Math.PI / 4))} wheelPrecision={50}
+            radius={14} minZ={0.001} lowerRadiusLimit={8} upperRadiusLimit={20} upperBetaLimit={Math.PI / 2} />
+            <hemisphericLight name='hemi' direction={new Vector3(0, -1, 0)} intensity={0.8} />
+
+            <directionalLight name="shadow-light" setDirectionToTarget={[Vector3.Zero()]} direction={Vector3.Zero()} position={new Vector3(-40, 30, -40)}
+              intensity={0.4} shadowMinZ={1} shadowMaxZ={2500}>
+              <shadowGenerator mapSize={1024} useBlurExponentialShadowMap={true} blurKernel={32} darkness={0.8} forceBackFacesOnly={true} depthScale={100} shadowCastChildren>
+                {Array.from(new Array(NUMBER_OF_BOXES), (_, index) => index).map(x => (
+                  <Sphere
+                    name={x.toFixed()}
+                    position={new Vector3(Math.cos(2 * Math.PI / NUMBER_OF_BOXES * x) * RADIUS, 3, Math.sin(2 * Math.PI / NUMBER_OF_BOXES * x) * RADIUS)}
+                    color={new Color3(Math.abs(x -(NUMBER_OF_BOXES/2)) / 10, Math.abs(x -(NUMBER_OF_BOXES/2)) / 10, Math.abs(x -(NUMBER_OF_BOXES/2)) / 10)}
+                  />
+                ))}
+              </shadowGenerator>
+            </directionalLight>
+
+            <ground name="ground1" width={24} height={24} subdivisions={2} receiveShadows={true}>
+              <physicsImpostor type={PhysicsImpostor.BoxImpostor} _options={{ mass: 0, restitution: 0.9 }} />
+            </ground>
+        </Scene>
+      </Engine>
+		</div>
+	)
+}
 
 export default App;
